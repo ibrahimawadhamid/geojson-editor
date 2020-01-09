b8566655c4476dcbcb34b557f9979a1f2d503525
Ibrahim Awad <ibrahim.a.hamid@gmail.com>
2020-01-09 14:05:50 +0300

    Updates

---
+++ precache-manifest.b754b645352d0eee216d87eafda3a46b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "730abaa121553317c866c7ca1dff3ef7",
+    "url": "/geojson-editor/index.html"
+  },
+  {
+    "revision": "c7a81e18bc9ff2fca13b",
+    "url": "/geojson-editor/static/css/2.feceb66d.chunk.css"
+  },
+  {
+    "revision": "4245ece04cdbcc2e0d58",
+    "url": "/geojson-editor/static/css/main.2b61b360.chunk.css"
+  },
+  {
+    "revision": "c7a81e18bc9ff2fca13b",
+    "url": "/geojson-editor/static/js/2.071d34b3.chunk.js"
+  },
+  {
+    "revision": "4245ece04cdbcc2e0d58",
+    "url": "/geojson-editor/static/js/main.a8d6a8c3.chunk.js"
+  },
+  {
+    "revision": "e981ac3d47a7d0a5e210",
+    "url": "/geojson-editor/static/js/runtime~main.9de181d6.js"
+  },
+  {
+    "revision": "8880a65c57d7f031579335be153f64a0",
+    "url": "/geojson-editor/static/media/marc.8880a65c.jpg"
+  },
+  {
+    "revision": "8e9f0a3c5578a20733d5bad0e51c91fb",
+    "url": "/geojson-editor/static/media/sidebar-1.8e9f0a3c.jpg"
+  },
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "/geojson-editor/static/media/sidebar-2.310509c9.jpg"
+  },
+  {
+    "revision": "2503169017014036cf0dd0dd7c2a2b8a",
+    "url": "/geojson-editor/static/media/sidebar-3.25031690.jpg"
+  },
+  {
+    "revision": "fc9cb0538eb5a4dfd6fbb1bf6dd6189b",
+    "url": "/geojson-editor/static/media/sidebar-4.fc9cb053.jpg"
+  }
+]);
